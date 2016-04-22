b8d4141b720d9f6a9672c26ec74f8ddf95704b6b
App Academy Student <student@appacademy.io>
2016-04-21 17:58:54 -0700

    add toy details

--- frontend/components/toy_detail.jsx
+++ frontend/components/toy_detail.jsx
@@ -3,37 +3,38 @@ var React = require('react'),
 
 var ToyDetail = React.createClass({
 
-  // getInitialState: function() {
-  //   return {
-  //     :
-  //   };
-  // },
+  getInitialState: function() {
+    return {
+      toy: this.getStateFromStore()
+    };
+  },
 
   _onChange: function() {
-
+    this.setState({toy: this.getStateFromStore()});
   },
 
   getStateFromStore: function() {
-    var pokeId = this.props.pokemonId;
-    var toyId = this.props.params.toyId;
-    PokemonStore.findToy(pokeId, toyId);
+    var pokeId = parseInt(this.props.params.pokemonId);
+    var toyId = parseInt(this.props.params.toyId);
+    return PokemonStore.findToy(pokeId, toyId);
   },
 
   componentDidMount: function() {
-
+    this.listenerToken = PokemonStore.addListener(this._onChange);
   },
 
   componentWillUnmount: function() {
-
+    this.listenerToken.remove();
   },
 
   componentWillReceiveProps: function(newProps) {
-
   },
 
   render: function() {
     return (
-      <div  className="detail" />
+      <div  className="detail">
+        <image src={this.state.toy.image_url}></image>
+      </div>
     );
   }
 
